0976ac60c2061fc47a7640d549988e716e19657d
kirychukyurii <me@kirychuk.pp.ua>
2025-05-25 19:30:18 +0300

    ref(actions/nfpm-build): add run action

--- actions/nfpm-build-action/dist/index.js
+++ actions/nfpm-build-action/dist/index.js
@@ -34045,5 +34045,10 @@ async function run() {
     }
 }
 
-export { run };
+/**
+ * The entrypoint for the action. This file simply imports and runs the action's
+ * main logic.
+ */
+/* istanbul ignore next */
+run();
 //# sourceMappingURL=index.js.map
